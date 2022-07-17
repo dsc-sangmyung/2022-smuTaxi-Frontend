7b6ee0fae85e59efabe5385eddf8673f047e6719
j1mmyson <bwson98@gmail.com>
2022-07-18 03:10:20 +0900

    Routes & Navigate 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Main from './components/Main';
+
 import "./App.css"
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Page1, Page2 } from './pages';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Page1/>}/>
+          <Route path="/go" element={<Page2 title="등교"/>}/>
+          <Route path="/back" element={<Page2 title="하교"/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Box.css";
 
-const Box = (props) => {
+const Box = () => {
     const [value, setValue] = useState(false);
     const [btn, setBtn] = useState('button_false');
+    const navigate = useNavigate();
 
     const handleGoChange = () => {
         setValue('등교');
@@ -21,6 +23,11 @@ const Box = (props) => {
             console.log("완료할 수 없습니다");
         } else {
             console.log(value, "를 선택하셨습니다.");
+        }
+        if(value === '등교') {
+            navigate('/go');
+        } else {
+            navigate('/back');
         }
     };
 
@@ -43,10 +50,12 @@ const Box = (props) => {
                 />
                 <p>하교</p>
             </div>
-
-            <button onClick={clickButton} className={btn}>
-                선택완료
-            </button>
+            
+            {/* <Link to="/pick"> */}
+                <button onClick={clickButton} className={btn}>
+                    선택완료
+                </button>
+            {/* </Link> */}
         </div>
     );
 };
