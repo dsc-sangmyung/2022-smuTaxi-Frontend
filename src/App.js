9f6dfa4125048b4916ecf82152a9607e57140047
leesoyuun <leesoyun4206@naver.com>
2022-07-28 02:02:53 +0900

    render_data_json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Page1, Page2, Page3, Page4, Page5} from './pages';
 
 
-
 function App() {
   return (
     <div className="App">
@@ -14,6 +13,7 @@ function App() {
           <Route path="/back" element={<Page3 title="하교"/>}/>
           <Route path="/time" element={<Page4 title="시간선택"/>}/>
           <Route path="/room" element={<Page5 title="시간선택"/>}/>
+          
         </Routes>
       </Router>
     </div>
