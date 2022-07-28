9515472cc3ef1c28d6d3e1ab55a6e076b6e812fa
leesoyuun <leesoyun4206@naver.com>
2022-07-28 23:24:13 +0900

    inside css부분 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css"
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+<<<<<<< Updated upstream
 import { Page1, Page2, Page3, Page4} from './pages';
+=======
+import { Page1, Page2, Page3, Page4, Page5,Page6} from './pages';
+>>>>>>> Stashed changes
 
 
 
@@ -14,6 +18,12 @@ function App() {
           <Route path="/go" element={<Page2 title="등교"/>}/>
           <Route path="/back" element={<Page3 title="하교"/>}/>
           <Route path="/time" element={<Page4 title="시간선택"/>}/>
+<<<<<<< Updated upstream
+=======
+          <Route path="/room" element={<Page5 title="시간선택"/>}/>
+          <Route path="/inside" element={<Page6/>}/>
+          
+>>>>>>> Stashed changes
         </Routes>
       </Router>
     </div>
---
+++ src/components/Room.js
@@ -0,0 +1,48 @@
+import {React, useState} from "react";
+import {useLocation} from "react-router";
+import { useNavigate } from "react-router-dom";
+import logoPath from '../components/arrow.png'
+import "./Box.css";
+import "./Time.css";
+import "./Room.css";
+
+const Room = () =>{
+    const location = useLocation();
+    const navigate = useNavigate();
+    
+    const clickCompleteButton = () => {
+        navigate('/inside',{
+            state : {
+                start : location.state.start,
+                dst:location.state.dst,
+                time : location.state.time
+            }
+        });
+    };
+    return(
+        <div className="box">
+            <div className="pin_height">
+            <div className="place">
+                <button>{location.state.start}</button>
+                <img src={logoPath}></img>
+                <button>{location.state.dst}</button>
+            </div>
+            <div className="FinalTime">{location.state.time}</div>
+            <div className="inner_box">
+                <div className="peopleNum">
+                    <button>4/4</button>
+                    <button>2/4</button>
+                    <button className="plus"> + </button>
+                </div>
+            </div>
+            </div>
+            <button onClick={clickCompleteButton} className="button_true">
+                선택완료
+            </button>
+        </div>
+
+        
+    )
+}
+
+export default Room;
