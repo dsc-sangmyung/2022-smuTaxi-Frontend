892fa209de8a3cb2d7c64afb9fd7da7e335d1d4a
leesoyuun <leesoyun4206@naver.com>
2023-10-25 20:18:51 +0900

    Refacto: Change css to style components and modify page 0

--- src/pages/Page0.js
+++ src/pages/Page0.js
@@ -1,12 +1,31 @@
-import React from "react";
-import Intro from "../components/Intro";
+import {React} from "react";
+import styled from 'styled-components';
+import * as c from "../components/Common/CommonStyle";
+import logoPath from '../components/intro.gif';
+import { useNavigate } from "react-router-dom";
 
-const Page0 = () => {
-    return (
-        <div className="main">
-            <Intro></Intro>
-        </div>
-    );
-};
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+const Page0 = () =>{
+    const navigate = useNavigate();
+
+    function Timer () {
+        setTimeout(() => {
+            navigate('./go_back');
+        }, 30000);
+    }
+
+    return(
+        <c.Totalframe>
+            <Center>
+                <img src={logoPath} onload={Timer()}></img>
+            </Center>
+        </c.Totalframe>
+    )
+}
 
 export default Page0;
